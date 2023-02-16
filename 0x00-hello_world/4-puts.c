476b6d78d35e3c0cf7ccad4747c4347b9ca05dd7
nouranhelal <nouran.helal212@gmail.com>
2023-02-16 15:21:53 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <studio.n>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	puts("\" Programming is like building a multilingual puzzle");
+	return (0);
+}
+
