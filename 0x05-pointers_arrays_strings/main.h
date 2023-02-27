f8ed1eb74ad77f0b83895a2d746a0645a12b1855
nouranhelal <nouran.helal212@gmail.com>
2023-02-27 15:23:31 -0800

    0x05. C - Pointers, arrays and strings alx

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <studio.h>
-#include <unistd.h>
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -15,6 +12,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _putchar(char c);
 
 #endif
